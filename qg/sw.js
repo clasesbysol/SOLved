d91423e355cf2bd4cbfba2d6a5b98c9b610a04b5
clasesbysol <clasesbysol@gmail.com>
2026-08-25 12:15:36 -0300

    Refresh ET27 PWA shell cache

--- qg/sw.js
+++ qg/sw.js
@@ -1,4 +1,4 @@
-const CACHE='qg-et27-shell-v2';
+const CACHE='qg-et27-shell-v3';
 const SHELL=['./','./index.html','./manifest.webmanifest'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)));self.skipWaiting()});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('qg-et27-shell-')&&key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]))});
@@ -7,7 +7,7 @@ self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
   if(url.origin!==self.location.origin||!url.pathname.startsWith('/SOLved/qg/'))return;
   if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
     return;
   }
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response})));
